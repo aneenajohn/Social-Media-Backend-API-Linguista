034b8045d88a0221ab019ab3007b68138ac16355
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-06-30 16:50:45 +0530

    Feat: added route for unfollow

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,30 @@
 const { extend } = require("lodash");
 const bcrypt = require("bcrypt");
+const User = require("../models/user.model");
+
+const getUser = async (req,res) => {
+    try{
+        const {user} = req;
+        const { password, updatedAt,createdAt, ...other } = user._doc;
+        res.json({
+            success: true,
+            user:other
+        })
+    }
+    catch (err) {
+        return res.status(500).json({
+            success: false, 
+            errorMessage: err.message,
+        });
+    }
+}
 
 const updateUserDetails = async (req,res) => {
     const {user} = req;
     const userFound = user;
     console.log({userFound});
-
-    if (!userFound.isAdmin) {
+    // user himself / admin can UPDATE
+    if (userFound._id || userFound.isAdmin) {
         if (req.body.password) {
             try {
               const salt = await bcrypt.genSalt(10);
@@ -38,7 +56,102 @@ const updateUserDetails = async (req,res) => {
             success:false,
             message:"You can update only your account!"
         });
-      }
+    }
 }
 
-module.exports = {updateUserDetails}
+const deleteUser = async (req,res) => {
+    try{
+        const {user} = req;
+        if (user._id || user.isAdmin) {
+            await user.remove();
+            res.json({
+                success: true,
+                message:"User deleted successfully",
+                deletedUser: user
+            })
+        }
+    }
+    catch (err) {
+        res.json({
+          success: false,
+          errorMessage: err.message,
+        });
+    }
+}
+
+const followUser = async (req, res) => {
+    const {user} = req
+    const {userId} = req.body; //follower to be added to user(user in param) = current user
+    // if both are not same users
+    if (userId !== user._id) {
+      try {
+        const currentUser = await User.findById(userId);
+        if (!user.followers.includes(userId)) {
+          await user.updateOne({ $push: { followers: userId } });
+          await currentUser.updateOne({ $push: { followings: user._id} }); 
+          res.status(200).json({
+              success:true,
+              message:"user has been followed",
+            //   followedUser:currentUser,
+            //   followingUser:user
+          });
+        } else {
+          res.status(403).json({
+              success:false,
+              message:"you allready follow this user"
+          });
+        }
+      } catch (err) {
+        res.status(500).json({
+            success: false,
+            errorMessage: err.message,
+        });
+      }
+    } else {
+      res.status(403).json({
+          success: false,
+          message:"you cant follow yourself"
+      });
+    }
+  }
+
+
+  const unfollowUser = async (req, res) => {
+    const {user} = req
+    const {userId} = req.body; //follower to be removed from user(user in param) = current user
+    // if both are not same users
+    if (userId !== user._id) {
+      try {
+        const currentUser = await User.findById(userId);
+        if (user.followers.includes(userId)) {
+          await user.updateOne({ $pull: { followers: userId } });
+          await currentUser.updateOne({ $pull: { followings: user._id} }); 
+          res.status(200).json({
+              success:true,
+              message:"user has been unfollowed",
+            //   unfollowedUser:currentUser,
+            //   unfollowingUser:user
+          });
+        } else {
+          res.status(403).json({
+              success:false,
+              message:"you dont follow this user"
+          });
+        }
+      } catch (err) {
+        res.status(500).json({
+            success: false,
+            errorMessage: err.message,
+        });
+      }
+    } else {
+      res.status(403).json({
+          success: false,
+          message:"you cant unfollow yourself"
+      });
+    }
+  }
+
+
+
+module.exports = {updateUserDetails,deleteUser,getUser,followUser,unfollowUser}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const userRouter = express.Router();
 const {userParamHandler} = require ("../controllers/params.controller");
-const {updateUserDetails} = require("../controllers/user.controller");
+const {getUser,updateUserDetails,deleteUser,followUser,unfollowUser} = require("../controllers/user.controller");
 
 userRouter.param("userId",userParamHandler);
 
 userRouter.route("/:userId")
-    .post(updateUserDetails);
+    .get(getUser)
+    .post(updateUserDetails)
+    .delete(deleteUser);
+
+userRouter.route("/:userId/follow")
+    .post(followUser)
+
+userRouter.route("/:userId/unfollow")
+    .post(unfollowUser)
 
 module.exports = { userRouter } ;
